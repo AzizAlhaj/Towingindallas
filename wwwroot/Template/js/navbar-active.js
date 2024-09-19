937df0deee262e20bffd6044419b1c23b9449d67
AzizAlhaj <aziz.alhaj96@gmail.com>
2024-09-19 21:27:06 +0300

    first commit

---
+++ wwwroot/Template/js/navbar-active.js
@@ -0,0 +1,19 @@
+﻿document.addEventListener('DOMContentLoaded', function () {
+    const currentUrl = window.location.pathname.toLowerCase();
+
+    document.querySelectorAll('.nav-item.nav-link, .dropdown-item').forEach(link => {
+        link.classList.remove('active');
+    });
+
+    document.querySelectorAll('.nav-item.nav-link, .dropdown-item').forEach(link => {
+        const activeValue = link.getAttribute('data-active');
+        
+        if (currentUrl.includes(activeValue) || (activeValue === 'home' && currentUrl === '/')) {
+            link.classList.add('active');
+        }
+    });
+
+    if (currentUrl.includes('/services')) {
+        document.getElementById('servicesNav').classList.add('active');
+    }
+});
---
+++ wwwroot/Template/js/services-dropdown.js
@@ -0,0 +1,10 @@
+﻿document.addEventListener('DOMContentLoaded', function () {
+    if (window.innerWidth > 760) {
+        document.getElementById('servicesNav').addEventListener('click', function (event) {
+            if (event.target === this) {
+                window.location.href = '/Services/Index';
+            }
+        });
+    }
+});
+
